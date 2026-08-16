07f38b5ca924043acd48bc9c345ce38979024719
Bhanu807474 <bhanumallikarjunas@gmail.com>
2026-08-16 15:25:34 +0530

    Nested-if basic program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/nestedif.java
@@ -0,0 +1,16 @@
+
+class nestedif {
+
+    public static void main(String[] args) {
+        int age = 20;
+        int weight = 70;
+
+        if (age >= 15) {
+            if (weight >= 50) {
+                System.out.println("Allow into cubbon park");
+
+            }
+        }
+
+    }
+}
