7e3758446273da23e77a8ea0630a8e9cfd182b2e
Bhanu807474 <bhanumallikarjunas@gmail.com>
2026-08-16 15:37:43 +0530

    compound

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/compound.java
@@ -0,0 +1,13 @@
+
+public class compound {
+
+    public static void main(String[] args) {
+        // Compound condition means combining two or more conditions using logical operators.
+        int age = 20;
+        if (age >= 16 && age <= 30) {
+            System.out.println("Allowed to drive");
+        } else {
+            System.out.println("Not allowed to drive");
+        }
+    }
+}
